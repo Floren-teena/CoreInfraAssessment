9950ae981a7cfeee373f1ab5377bf591634977d5
Floren-teena <florentinaantigha27@gmail.com>
2024-09-01 16:14:46 +0100

    Initial commit

---
+++ CoreInfraAssessment/Controllers/DashboardController/DashboardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreInfraAssessment.Controllers.DashboardController
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
